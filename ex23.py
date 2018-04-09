bc339cf4c7717085b70afbb12646859a795d1a47
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-04-08 22:21:13 -0400

    fixed syntax

--- ex23.py
+++ ex23.py
@@ -10,7 +10,7 @@ def main(language_file, encoding, errors):
 		return main(language_file, encoding, errors)
 
 
-def print_line(line, encoding, errors)
+def print_line(line, encoding, errors):
 	next_lang = line.strip()
 	raw_bytes = next_lang.encode(encoding, errors=errors)
 	cooked_string = raw_bytes.decode(encoding, errors=errors)
