1b89aa2616f81f9c706a8f11be87a1d316ef9012
Tyler Gunter <tyler.james.gunter@gmail.com>
2018-04-17 22:43:32 -0400

    fixed spelling error

--- ex24.py
+++ ex24.py
@@ -6,7 +6,7 @@ poem = """
 \tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
-nor coprehend passion from intuition
+nor comprehend passion from intuition
 and requires an explanation
 \n\t\twhere there is none.
 """
